e5aaea993da2a51b1866490ead49c6eaf4ad8336
Lucvoice <simkondabrand@gmail.com>
2026-05-09 09:15:26 +0300

    Update qr.js

--- routes/qr.js
+++ routes/qr.js
@@ -62,7 +62,7 @@ router.get('/', async (req, res) => {
                             <!DOCTYPE html>
                             <html>
                             <head>
-                                <title>GIFTED-MD | QR CODE</title>
+                                <title>LUKA-AI | QR CODE</title>
                                 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
                                 <style>
                                     body {
